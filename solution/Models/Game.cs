4a3afbb9aa2bf9942f3edc56534576b0f9d28146
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-22 14:05:05 -0700

    edit model

--- solution/Models/Game.cs
+++ solution/Models/Game.cs
@@ -1,11 +1,23 @@
 namespace CrossPlay.Models
 {
-    public class Game
-    {
-        public int AnimalId { get; set; }
-        public string Name { get; set; }
-        public string Species { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
-    }
+  public class Game
+  {
+    public int GameId { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Link { get; set; }
+    public bool Windows { get; set; }
+    public bool Playstation { get; set; }
+    public bool XboxOne { get; set; }
+    public bool Mac { get; set; }
+    public bool Switch { get; set; }
+    public bool PlaystationVita { get; set; }
+    public bool Playstation3 { get; set; }
+    public bool IOS { get; set; }
+    public bool Android { get; set; }
+    public bool XboxSeriesSX { get; set; }
+    public bool Playstation5 { get; set; }
+    public bool Linux { get; set; }
+    public bool Wii { get; set; }
+  }
 }
