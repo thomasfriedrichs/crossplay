e331155ca620e842c95afa463174760ce870af05
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-23 11:51:39 -0700

    edit models

---
+++ solution/Models/GameConsole.cs
@@ -0,0 +1,9 @@
+namespace CrossPlay.Models
+{
+  public class GameConsole
+  {
+    public int GameConsoleId { get; set; }
+    public int GameId { get; set; }
+    public int ConsoleId { get; set; }
+  }
+}
