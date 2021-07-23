f9a0e43d69a31f684a217c23cb9035f78107a702
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-23 11:28:56 -0700

    seed consoles

--- solution/Models/CrossPlayContext.cs
+++ solution/Models/CrossPlayContext.cs
@@ -41,6 +41,46 @@ namespace CrossPlay.Models
             new Console {
               Name = "Mac",
               ConsoleId = 4
+            },
+            new Console {
+              Name = "Playstation 2",
+              ConsoleId = 5
+            },
+            new Console {
+              Name = "Switch",
+              ConsoleId = 5
+            },
+            new Console {
+              Name = "Playstation Vita 6",
+              ConsoleId = 6
+            },
+            new Console {
+              Name = "Playstation 3",
+              ConsoleId = 7
+            },
+            new Console {
+              Name = "iOS",
+              ConsoleId = 8
+            },
+            new Console {
+              Name = "Android",
+              ConsoleId = 9
+            },
+            new Console {
+              Name = "Xbox Series X/S",
+              ConsoleId = 10
+            },
+            new Console {
+              Name = "Playstation 5",
+              ConsoleId = 11
+            },
+            new Console {
+              Name = "Linux",
+              ConsoleId = 12
+            },
+            new Console {
+              Name = "Wii",
+              ConsoleId = 13
             }
           );
         }
