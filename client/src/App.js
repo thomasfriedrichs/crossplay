64e6f8d7337921a4e5c42e5fd9958250596db596
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-26 13:36:07 -0700

    edit routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+// Pages
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+import Category from './pages/Category';
+import Game from './pages/Game';
+import Games from './pages/Games';
+import Platform from './pages/Platform';
+import Platforms from './pages/Platforms';
+// Components
 import Navbar from './components/nav/Navbar';
+import Footer from './components/Nav/Footer'; 
 
 function App() {
   return (
     <div className = 'app'>
-      <Navbar/>
-
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route>
+            
+          </Route>
+        </Switch>
+        <Footer/>
+      </Router>
     </div>
   );
 }
