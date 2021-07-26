a58587548931a1feda3fee45fa9a30561e9b0772
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-26 09:06:45 -0700

    add relational tables

--- solution/Models/CrossPlayContext.cs
+++ solution/Models/CrossPlayContext.cs
@@ -217,6 +217,414 @@ namespace CrossPlay.Models
               ConsoleId = 13
             }
           );
+
+          builder.Entity<GameCategory>().HasData(
+            new GameCategory
+            {
+              GameCategoryId = 1,
+              GameId = 1,
+              CategoryId = 4
+            },
+            new GameCategory
+            {
+              GameCategoryId = 2,
+              GameId = 2,
+              CategoryId = 2
+            },
+            new GameCategory
+            {
+              GameCategoryId = 3,
+              GameId = 3,
+              CategoryId = 2
+            },
+            new GameCategory
+            {
+              GameCategoryId = 4,
+              GameId = 4,
+              CategoryId = 2
+            },
+            new GameCategory
+            {
+              GameCategoryId = 5,
+              GameId = 5,
+              CategoryId = 2
+            },
+            new GameCategory
+            {
+              GameCategoryId = 6,
+              GameId = 6,
+              CategoryId = 1
+            },
+            new GameCategory
+            {
+              GameCategoryId = 7,
+              GameId = 7,
+              CategoryId = 8
+            },
+            new GameCategory
+            {
+              GameCategoryId = 8,
+              GameId = 8,
+              CategoryId = 5
+            },
+            new GameCategory
+            {
+              GameCategoryId = 9,
+              GameId = 9,
+              CategoryId = 1
+            },
+            new GameCategory
+            {
+              GameCategoryId = 10,
+              GameId = 10,
+              CategoryId = 1
+            },
+            new GameCategory
+            {
+              GameCategoryId = 11,
+              GameId = 11,
+              CategoryId = 3
+            },
+            new GameCategory
+            {
+              GameCategoryId = 12,
+              GameId = 12,
+              CategoryId = 3
+            },
+            new GameCategory
+            {
+              GameCategoryId = 13,
+              GameId = 13,
+              CategoryId = 1
+            },
+            new GameCategory
+            {
+              GameCategoryId = 14,
+              GameId = 14,
+              CategoryId = 1
+            },
+            new GameCategory
+            {
+              GameCategoryId = 15,
+              GameId = 15,
+              CategoryId = 1
+            }          
+          );
+
+          builder.Entity<GameConsole>().HasData(
+            new GameConsole
+            {
+              GameConsoleId = 1,
+              GameId = 1,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 2,
+              GameId = 1,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 3,
+              GameId = 1,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 4,
+              GameId = 1,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 5,
+              GameId = 1,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 6,
+              GameId = 2,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 7,
+              GameId = 2,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 8,
+              GameId = 2,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 9,
+              GameId = 3,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 10,
+              GameId = 3,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 11,
+              GameId = 4,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 12,
+              GameId = 4,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 13,
+              GameId = 4,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 14,
+              GameId = 5,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 15,
+              GameId = 5,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 16,
+              GameId = 5,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 17,
+              GameId = 6,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 18,
+              GameId = 6,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 19,
+              GameId = 6,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 20,
+              GameId = 6,
+              ConsoleId = 5
+            },
+            new GameConsole
+            {
+              GameConsoleId = 21,
+              GameId = 6,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 22,
+              GameId = 6,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 23,
+              GameId = 7,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 24,
+              GameId = 7,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 25,
+              GameId = 7,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 26,
+              GameId = 8,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 27,
+              GameId = 8,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 28,
+              GameId = 8,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 29,
+              GameId = 9,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 30,
+              GameId = 9,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 31,
+              GameId = 10,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 32,
+              GameId = 10,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 33,
+              GameId = 10,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 34,
+              GameId = 10,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 35,
+              GameId = 10,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 36,
+              GameId = 11,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 37,
+              GameId = 11,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 38,
+              GameId = 11,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 39,
+              GameId = 11,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 40,
+              GameId = 12,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 41,
+              GameId = 12,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 42,
+              GameId = 12,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 43,
+              GameId = 13,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 44,
+              GameId = 13,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 45,
+              GameId = 13,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 46,
+              GameId = 14,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 47,
+              GameId = 14,
+              ConsoleId = 2
+            },
+            new GameConsole
+            {
+              GameConsoleId = 48,
+              GameId = 14,
+              ConsoleId = 11
+            },
+            new GameConsole
+            {
+              GameConsoleId = 49,
+              GameId = 14,
+              ConsoleId = 3
+            },
+            new GameConsole
+            {
+              GameConsoleId = 50,
+              GameId = 14,
+              ConsoleId = 10
+            },
+            new GameConsole
+            {
+              GameConsoleId = 51,
+              GameId = 15,
+              ConsoleId = 1
+            },
+            new GameConsole
+            {
+              GameConsoleId = 52,
+              GameId = 15,
+              ConsoleId = 2
+            }
+          );
         }
     }
 }
