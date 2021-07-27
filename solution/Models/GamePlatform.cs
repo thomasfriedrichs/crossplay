d11d7ec33b48794613c82572e12f330087fcc0d9
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-27 09:40:42 -0700

    edit model

--- solution/Models/GamePlatform.cs
+++ solution/Models/GamePlatform.cs
@@ -5,5 +5,7 @@ namespace CrossPlay.Models
     public int GamePlatformId { get; set; }
     public int GameId { get; set; }
     public int PlatformId { get; set; }
+    public virtual Game Game { get; set; }
+    public virtual Platform Platform { get; set; }
   }
 }
