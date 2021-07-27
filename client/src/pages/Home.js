5e5fb436ca89e289143a4f0608aab3c6dc840541
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-27 14:27:51 -0700

    troubleshoot:

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {getAllGames, findByTitle}  from "../services /Service";
 
 
 const Home = () => {
+  // const [getAllGames]
+
   return (
     <div>
-      
+
     </div>
   )
 };
