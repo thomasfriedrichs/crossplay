61a24ab913f37da18648b33c0f098f514a872c92
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-27 15:51:23 -0700

    troubleshoot

--- client/src/pages/Games.js
+++ client/src/pages/Games.js
@@ -3,6 +3,16 @@ import { Link } from "react-router-dom";
 import getAllGames  from '../services/Service';
 
 const Games = () => {
+  // const retrieveGames = () => {
+  //   getAllGames()
+  //     .then(response => {
+  //       setGames(response.data);
+  //       console.log(response.data);
+  //     })
+  //     .catch(e => {
+  //       console.log(e);
+  //     });
+  // };
 
   console.log(getAllGames());
 
