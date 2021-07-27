104c50c34b241ec169c2302a026f26e62898dc79
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-27 11:31:42 -0700

    add controller

---
+++ solution/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CrossPlay.Models;
+
+namespace CrossPlay.Controllers
+{
+  [Route("api/crossplay/categories")]
+  [ApiController]
+  public class CategoriesController : ControllerBase
+  {
+    private readonly CrossPlayContext _db;
+    public CategoriesController(CrossPlayContext db)
+    {
+      _db = db;
+    }
+
+    //Get: api/crossplay/categorys
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+    {
+      return await _db.Categories.ToListAsync();
+    } 
+
+    //Get: api/crossplay/categorys/1
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Category>> GetCategory(int id)
+    {
+        var category = await _db.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return category;
+    }
+  }
+}
