2f8166186b4d6e341d8adc16a0000d78e6d6a090
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-27 21:13:15 -0700

    testing axios refactor

--- client/src/pages/Games.js
+++ client/src/pages/Games.js
@@ -6,15 +6,19 @@ import axios from 'axios';
 
 const Games = () => {
   const [post, setPost] = React.useState(null);
-  const [error, setError] = React.useState(null);
+  //const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
+    axios.get(`${baseUrl}/games`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
 
-  })
+  if (!post) return null;
 
   return (
     <div>
-
+      <h1>{post.GameName}</h1>
     </div>
   )
 };
